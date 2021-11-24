764272232b61af9b2eadc42106c793fb637303c2
sky_bear <wangpeng@tzdtz.com>
2021-11-24 18:34:09 +0800

    feat: 基础数据类型

---
+++ basics/DataType.java
@@ -0,0 +1,25 @@
+public class DataType {
+  public static void main(String[] args) {
+    byte a =1;
+    System.out.println(a);
+  }
+
+}
+
+// bit 和 byte
+// bit 一个二进制的位叫做一个bit,网络宽带中的单位，都是bit
+// 八个二级制的位，组成一个byte.硬盘等存储的单位，都是byte
+// byte 是计算机中基本的衡量存储的单位，计算机在对外使用时不会用bit作为划分存储的单位
+// 整数类型
+// byte  值域：-128~127
+// short 占用2个byte 值域：-32768-32767
+// int 占用4个byte
+// long 占用8个btye 
+
+// 浮点（小数）
+// float 占用4个byte，有精度，值域复杂
+// double 精度是float的两倍，占用8个byte 
+
+// 布尔和字符串数据类型
+// boolean 占用1个byte 值域时true，false
+// char 占用2个byte,值域时所有字符
